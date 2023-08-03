77480f1cb83af6850589a097e33cf297e538cc6b
rlawldns12 <101171594+rlawldns12@users.noreply.github.com>
2023-08-03 16:42:53 +0900

    코딩애플 part3

--- for_ex.js
+++ for_ex.js
@@ -1,4 +1,4 @@
-var 출석부 = ['흥민', '영희', '철수', '재석'];
+/*var 출석부 = ['흥민', '영희', '철수', '재석'];
 
 function 이름찾기(a){
     for(let i = 0 ; i < 출석부.length; i++){
@@ -8,4 +8,34 @@ function 이름찾기(a){
     }
 }
 
-이름찾기('명수')
+이름찾기('재석');
+
+
+function 구구단(){
+    for(let i = 2; i <= 9; i++){
+        for(let j = 1; j <= 9;   j++){
+            console.log(i * j)
+        }
+    }
+};
+
+
+구구단()
+*/
+
+
+function 평균구하기(arr,b){
+    let result = 0;
+    for(let i = 0; i < arr.length; i++){
+        result += arr[i]; // arr값을 다 더해줌
+    }
+    result = result / arr.length;
+    if(result < b){
+    console.log(`평균 보다 ${b - result}점이 올랐네요`);
+    }
+    else{
+        console.log(`평균 보다 ${result - b}점이 떨어졌네요 재수 ㄱㄱ`);
+    }
+}
+
+평균구하기([40,40,40],20)
